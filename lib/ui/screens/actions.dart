074b2355327c1834ef18ed54f29386c8ec857154
avive <aviveyal07@gmail.com>
2023-03-22 17:33:19 +0200

    bump version

--- lib/ui/screens/actions.dart
+++ lib/ui/screens/actions.dart
@@ -195,7 +195,7 @@ class _ActionsScreenState extends State<ActionsScreen> {
         children: <CupertinoListTile>[
           CupertinoListTile.notched(
             title: const Text('Blockchain info'),
-            leading: const Icon(CupertinoIcons.building_2_fill, size: 24),
+            leading: const Icon(CupertinoIcons.building_2_fill, size: 28),
             trailing: const CupertinoListTileChevron(),
             onTap: () => context.push(ScreenPaths.karmaChain),
           ),
