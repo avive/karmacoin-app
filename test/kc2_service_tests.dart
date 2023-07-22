f885fdff9e299e15abccfdd1bdde1fd6b0d58919
avive <aviveyal07@gmail.com>
2023-07-22 09:24:17 +0300

    Update signup test to demonstrate current kc issue

--- test/kc2_service_tests.dart
+++ test/kc2_service_tests.dart
@@ -6,6 +6,7 @@ import 'package:karma_coin/logic/kc2/identity.dart';
 import 'package:karma_coin/logic/kc2/identity_interface.dart';
 import 'package:karma_coin/services/v2.0/kc2.dart';
 import 'package:karma_coin/services/v2.0/kc2_service.dart';
+import 'package:karma_coin/services/v2.0/types.dart';
 
 void main() {
   group('kc2_api', () {
@@ -22,20 +23,48 @@ void main() {
 
       // Create a new identity for local user
       IdentityInterface katya = Identity();
-      IdentityInterface punch = Identity();
 
       await katya.initNoStorage();
-      await punch.initNoStorage();
 
       // Set katya as signer
       kc2Service.setKeyring(katya.keyring);
       debugPrint('Local user katya public address: ${katya.accountId}');
-      debugPrint('Punch 2nd user public address: ${punch.accountId}');
 
       final completer = Completer<bool>();
 
       kc2Service.newUserCallback = (tx) async {
         debugPrint('>> new user callback called');
+        if (tx.failedReason != null) {
+          completer.complete(false);
+          return;
+        }
+
+        // all 3 methods should return's Katya's account data
+        KC2UserInfo? userInfo =
+            await kc2Service.getUserInfoByAccountId(katya.accountId);
+
+        if (userInfo == null) {
+          debugPrint('Faied to get user info by account id');
+          completer.complete(false);
+          return;
+        }
+
+        userInfo = await kc2Service.getUserInfoByPhoneNumberHash(
+            kc2Service.getPhoneNumberHash("972549805380"));
+
+        if (userInfo == null) {
+          debugPrint('Faied to get user info by phone number');
+          completer.complete(false);
+          return;
+        }
+
+        userInfo = await kc2Service.getUserInfoByUsername("Katya");
+        if (userInfo == null) {
+          debugPrint('Faied to get user info by nickname');
+          completer.complete(false);
+          return;
+        }
+
         completer.complete(true);
       };
 
