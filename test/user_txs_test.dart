bcf57f850ef56fabd09580543be3c79f59eef864
HolyGrease <kireevdanil2000@gmail.com>
2023-08-02 11:09:37 +0300

    fix after wrong referral reward

--- test/user_txs_test.dart
+++ test/user_txs_test.dart
@@ -100,11 +100,10 @@ void main() {
                       expect(punch.outgoingAppreciations.value.length, 2);
 
                       // check karma score and balance
-                      // @Avive balance equal to `20000000` because of referral reward
                       expect(punch.userInfo.value?.balance,
-                          BigInt.from(20000000 + 1234 - 12345 - 54321));
+                          BigInt.from(10000000 + 1234 - 12345 - 54321));
 
-                      expect(punch.userInfo.value?.karmaScore, 4);
+                      expect(punch.userInfo.value?.karmaScore, 3);
 
                       await punch.signout();
                       completer.complete(true);
