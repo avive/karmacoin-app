fd486bdcd27a98f7b79be2216088100b534653ad
HolyGrease <kireevdanil2000@gmail.com>
2023-08-15 20:54:42 +0300

    fix leader board data class

--- lib/services/v2.0/types.dart
+++ lib/services/v2.0/types.dart
@@ -125,25 +125,14 @@ class Contact {
 }
 
 class LeaderBoardEntry {
-  String username;
   String accountId;
-  String phoneNumberHash;
-  List<CommunityMembership> communityMemberships;
-  List<TraitScore> traitScores;
+  int score;
 
-  LeaderBoardEntry(this.username, this.accountId, this.phoneNumberHash,
-      this.communityMemberships, this.traitScores);
+  LeaderBoardEntry(this.accountId, this.score);
 
   LeaderBoardEntry.fromJson(Map<String, dynamic> json)
-    : username = json['user_name'],
-      accountId = json['account_id'],
-      phoneNumberHash = json['phone_number_hash'],
-      communityMemberships = (json['community_membership'] as List<dynamic>)
-        .map((e) => CommunityMembership.fromJson(e))
-        .toList(),
-      traitScores = (json['trait_scores'] as List<dynamic>)
-        .map((e) => TraitScore.fromJson(e))
-        .toList();
+    : accountId = json['account_id'],
+      score = json['score'];
 }
 
 enum VerificationResult {
