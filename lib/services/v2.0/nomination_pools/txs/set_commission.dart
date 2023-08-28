b4ab11d24a502f5b1c8d54cbf8629d9dc74f52b7
HolyGrease <kireevdanil2000@gmail.com>
2023-08-28 09:15:06 +0300

    improve doc

--- lib/services/v2.0/nomination_pools/txs/set_commission.dart
+++ lib/services/v2.0/nomination_pools/txs/set_commission.dart
@@ -1,7 +1,9 @@
 import 'package:karma_coin/services/v2.0/nomination_pools/types.dart';
 import 'package:karma_coin/services/v2.0/txs/tx.dart';
 
-/// @HolyGrease - please add code doc for this tx - what is it used for? what is the use case?
+/// Set the commission of a pool. Pool as a validator can set its own commission.
+/// The commission is a percentage of the reward that will be taken by the pool.
+/// The beneficiary is the account that will receive the commission.
 class KC2SetCommissionTxV1 extends KC2Tx {
   PoolId poolId;
   int? commission;
