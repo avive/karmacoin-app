6f115a5fc03ab27fdab968442fade23ae73fe214
avive <aviveyal07@gmail.com>
2023-09-14 19:30:19 +0300

    fix tx details screen bug

--- lib/ui/screens/main/user_home.dart
+++ lib/ui/screens/main/user_home.dart
@@ -444,7 +444,7 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
       color: CupertinoColors.black, // This is required
       title: 'Karma Coin - Home',
       child: CupertinoPageScaffold(
-        resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: true,
         child: CustomScrollView(
             physics: const NeverScrollableScrollPhysics(),
             slivers: [
