9bb0bfde020bbf8c236214f8067a61f18de78753
avive <aviveyal07@gmail.com>
2023-09-18 21:05:12 +0300

    Fix infinite scrolling in contact selection

--- lib/ui/screens/actions/users_browser.dart
+++ lib/ui/screens/actions/users_browser.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:karma_coin/data/genesis_config.dart';
 import 'package:karma_coin/data/personality_traits.dart';
 import 'package:karma_coin/services/v2.0/types.dart';
@@ -8,7 +9,7 @@ import 'package:karma_coin/ui/components/pill.dart';
 import 'package:random_avatar/random_avatar.dart';
 import 'package:status_alert/status_alert.dart';
 
-enum FetchStatus { idle, loading, loaded, error }
+const _pageSize = 30;
 
 // Karma coin users selector
 class KarmaCoinUserSelector extends StatefulWidget {
@@ -32,303 +33,128 @@ class KarmaCoinUserSelector extends StatefulWidget {
 }
 
 class _KarmaCoinUserSelectorState extends State<KarmaCoinUserSelector> {
+  final PagingController<int, Contact> _pagingController =
+      PagingController(firstPageKey: 0);
+
   // we assume api is available until we know otherwise
-  bool apiOffline = false;
-  List<Contact> contacts = [];
-  late TextEditingController textController;
+  late TextEditingController _textController;
   // limit per request
-  final limit = 30;
-  final localUserName = kc2User.userInfo.value?.userName;
-  final localUserPhoneNumberHash = kc2User.userInfo.value?.phoneNumberHash;
-  FetchStatus featchStatus = FetchStatus.idle;
-  late String? lastPrefix;
-  final ScrollController scrollController = ScrollController();
-  bool additionalResultsAvailable = false;
-  //
-  //
-  int lastRequestFromIndex = 0;
-  String lastRequestedPrefix = '';
+  final _localUserName = kc2User.userInfo.value?.userName;
+  final _localUserPhoneNumberHash = kc2User.userInfo.value?.phoneNumberHash;
+  String _searchTerm = '';
+  PagingStatus _pagingStatus = PagingStatus.loadingFirstPage;
 
-  void setFetchState(FetchStatus status) {
-    setState(() {
-      featchStatus = status;
-    });
-  }
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final List<Contact> results = await kc2Service.getContacts(_searchTerm,
+          fromIndex: pageKey, limit: _pageSize);
 
-  void _loadAdditionalContacts() {
-    if (additionalResultsAvailable) {
-      debugPrint('Loading more contacts on scroll...');
-      _loadContacts(lastPrefix!);
-    } else {
-      debugPrint('No more results to load on scrol...');
-    }
-  }
+      // if there are less results than limit, we know there are no more results to fetch
+      bool additionalResultsAvailable =
+          results.length < _pageSize ? false : true;
 
-  /// Load more contacts from the server for a prefix
-  void _loadContacts(String prefix) {
-    prefix = prefix.toLowerCase();
+      // cleanup results
+      results.removeWhere((contact) =>
+          contact.userName.trim().isEmpty ||
+          contact.userName == _localUserName ||
+          contact.phoneNumberHash == _localUserPhoneNumberHash);
 
-    if (prefix != lastRequestedPrefix) {
-      // change of prefix - reset index and contacts
-      setState(() {
-        lastRequestedPrefix = prefix;
-        lastRequestFromIndex = 0;
-        contacts.clear();
-      });
-    }
-
-    Future.delayed(Duration.zero, () async {
-      try {
-        debugPrint('getting contatcs prefix $prefix');
-        setFetchState(FetchStatus.loading);
-        List<Contact> results = await kc2Service.getContacts(prefix,
-            fromIndex: lastRequestFromIndex, limit: limit);
-
-        // if there are less results than limit, we know there are no more results to fetch
-        additionalResultsAvailable = results.length < limit ? false : true;
-
-        lastRequestFromIndex += results.length;
-
-        // cleanup results
-        results.removeWhere((contact) =>
-            contact.userName.trim().isEmpty ||
-            contact.userName == localUserName ||
-            contact.phoneNumberHash == localUserPhoneNumberHash);
-
-        debugPrint('got ${results.length} contacts');
-
-        setState(() {
-          apiOffline = false;
-          featchStatus = FetchStatus.loaded;
-          contacts.addAll(results);
-        });
-      } catch (e) {
-        setState(() {
-          apiOffline = true;
-          featchStatus = FetchStatus.error;
-        });
-        if (!mounted) return;
-        StatusAlert.show(context,
-            duration: const Duration(seconds: 2),
-            title: 'Server Error',
-            subtitle: 'Please try later',
-            configuration: const IconConfiguration(
-                icon: CupertinoIcons.exclamationmark_triangle),
-            dismissOnBackgroundTap: true,
-            maxWidth: statusAlertWidth);
-        debugPrint('error getting contacts: $e');
+      if (!additionalResultsAvailable) {
+        _pagingController.appendLastPage(results);
+      } else {
+        final nextPageKey = pageKey + results.length;
+        _pagingController.appendPage(results, nextPageKey);
       }
-    });
+    } catch (error) {
+      _pagingController.error = error;
+      if (!context.mounted) return;
+      StatusAlert.show(context,
+          duration: const Duration(seconds: 2),
+          title: 'Server Error',
+          subtitle: 'Please try later',
+          configuration: const IconConfiguration(
+              icon: CupertinoIcons.exclamationmark_triangle),
+          dismissOnBackgroundTap: true,
+          maxWidth: statusAlertWidth);
+      debugPrint('error getting contacts: $e');
+    }
   }
 
   @override
   void dispose() {
-    scrollController.dispose();
-    textController.dispose();
+    _textController.dispose();
+    _pagingController.dispose();
     super.dispose();
   }
 
   @override
   initState() {
     super.initState();
-    apiOffline = false;
-    textController = TextEditingController();
-    scrollController.addListener(_loadAdditionalContacts);
-    _loadContacts('');
+    _textController = TextEditingController();
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+
+    _pagingController.addStatusListener((status) {
+      setState(() {
+        _pagingStatus = status;
+      });
+
+      if (status == PagingStatus.subsequentPageError) {
+        // todo: show error
+      }
+    });
+  }
+
+  void _updateSearchTerm(String searchTerm) {
+    _searchTerm = searchTerm;
+    _pagingController.refresh();
   }
 
   Widget _getBodyContent(BuildContext context) {
-    if (apiOffline == true || featchStatus == FetchStatus.error) {
-      return Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24),
-        child: Center(
-          child: Text('Server is down.\n\nPlease try again later.',
-              textAlign: TextAlign.center,
-              style: CupertinoTheme.of(context).textTheme.pickerTextStyle),
-        ),
-      );
-    }
-
-    List<Widget> widgets = [];
-
-    widgets.add(
-      Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24, top: 12),
-        child: CupertinoSearchTextField(
-            controller: textController,
-            autofocus: true,
-            autocorrect: false,
-            keyboardType: TextInputType.text,
-            placeholder: 'Enter a user name',
-            onChanged: (value) {
-              _loadContacts(value);
-            },
-            style: CupertinoTheme.of(context).textTheme.pickerTextStyle),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        _getSearchBar(context),
+        CustomScrollView(
+          slivers: <Widget>[
+            _getContactsList(context),
+          ],
+        )
+      ],
     );
+  }
 
-    if (contacts.isEmpty && featchStatus == FetchStatus.loaded) {
-      widgets.add(
-        Padding(
-          padding: const EdgeInsets.only(top: 64, bottom: 36),
-          child: Center(
-            child: Text('😞 No matching users found',
-                textAlign: TextAlign.center,
-                style: CupertinoTheme.of(context).textTheme.pickerTextStyle),
-          ),
-        ),
-      );
-    } else if (featchStatus == FetchStatus.loading) {
-      widgets.add(
-        Padding(
-          padding: const EdgeInsets.only(top: 64, bottom: 36),
-          child: Center(
-            child: Text('⌛️ One sec please...',
-                textAlign: TextAlign.center,
-                style: CupertinoTheme.of(context).textTheme.pickerTextStyle),
-          ),
-        ),
-      );
-    } else {
-      widgets.add(_getContactsList(context));
-    }
-
-    return Padding(
-      padding: const EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 24),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: widgets),
-    );
+  Widget _getSearchBar(BuildContext context) {
+    return SliverToBoxAdapter(
+        child: Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
+      child: CupertinoSearchTextField(
+          controller: _textController,
+          autofocus: true,
+          autocorrect: false,
+          keyboardType: TextInputType.text,
+          placeholder: 'Enter a user name',
+          onChanged: (value) {
+            _updateSearchTerm(value);
+          },
+          style: CupertinoTheme.of(context).textTheme.pickerTextStyle),
+    ));
   }
 
   Widget _getContactsList(BuildContext context) {
-    return Expanded(
-      child: ListView.separated(
-        padding: const EdgeInsets.only(top: 24),
-        separatorBuilder: (context, index) {
-          return const Divider(
-            thickness: 1,
-            indent: 0,
-          );
-        },
-        controller: scrollController,
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          return _getContactWidget(context, contacts[index], index);
-        },
-      ),
-    );
-  }
-
-  void _setAdmin(Contact contact) {
-    // todo: move this to kc2
-    /*
-    Future.delayed(Duration.zero, () async {
-      try {
-        KarmaCoinUser localUser = accountLogic.karmaCoinUser.value!;
-        ed.KeyPair keyPair = accountLogic.keyPair.value!;
-
-        SetCommunityAdminData data = SetCommunityAdminData(
-          communityId: widget.communityId,
-          targetAccountId: contact.accountId,
-          timestamp: Int64(DateTime.now().millisecondsSinceEpoch),
-          admin: true,
-        );
-
-        List<int> message = data.writeToBuffer();
-
-        Uint8List edSignature =
-            ed.sign(keyPair.privateKey, Uint8List.fromList(message));
-
-        SetCommunityAdminRequest request = SetCommunityAdminRequest(
-            fromAccountId: localUser.userData.accountId,
-            data: message,
-            signature: edSignature);
-
-        await api.apiServiceClient.setCommunityAdmin(request);
-        if (!mounted) return;
-        StatusAlert.show(context,
-            duration: const Duration(seconds: 4),
-            title: 'Allright',
-            subtitle: 'User is now an admin',
-            configuration: const IconConfiguration(icon: CupertinoIcons.smiley),
-            dismissOnBackgroundTap: true,
-            maxWidth: statusAlertWidth);
-      } catch (e) {
-        debugPrint('error setting admin: $e');
-        if (!mounted) return;
-        StatusAlert.show(context,
-            duration: const Duration(seconds: 4),
-            title: 'Ooops',
-            subtitle: 'Server error',
-            configuration: const IconConfiguration(
-                icon: CupertinoIcons.exclamationmark_triangle),
-            dismissOnBackgroundTap: true,
-            maxWidth: statusAlertWidth);
-        return;
-      }
-    });*/
-  }
-
-  void _showContextMenu(BuildContext conext, int index) {
-    debugPrint('showing context menu for index $index');
-
-    Contact contact = contacts[index];
-
-    showCupertinoModalPopup<void>(
-      context: context,
-      builder: (BuildContext context) => CupertinoActionSheet(
-        title: Text(
-          'Make user ${contact.userName} a community admin?',
-          style: CupertinoTheme.of(context)
-              .textTheme
-              .dateTimePickerTextStyle
-              .merge(
-                const TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-        ),
-        actions: <CupertinoActionSheetAction>[
-          CupertinoActionSheetAction(
-            onPressed: () {
-              _setAdmin(contact);
-              Navigator.pop(context);
-            },
-            child: const Text('Yes'),
-          ),
-          CupertinoActionSheetAction(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            isDefaultAction: true,
-            child: const Text('No'),
-          ),
-        ],
+    return PagedSliverList<int, Contact>(
+      pagingController: _pagingController,
+      builderDelegate: PagedChildBuilderDelegate<Contact>(
+        itemBuilder: (context, item, index) =>
+            _getContactWidget(context, item, index),
       ),
     );
   }
 
   Widget _getContactWidget(BuildContext context, Contact contact, int index) {
-    // todo: add personality trait emojis from appreciation
-    // show appreciations strip for user :-)
-    // String phoneNumber = '+${contact.mobileNumber.number.formatPhoneNumber()}';
     String displayName =
         '${contact.userName} ${contact.getCommunitiesBadge()}'.trim();
-
-    bool isAdmin = false;
-
-    for (CommunityMembership m in contact.communityMemberships) {
-      Community? c = GenesisConfig.communities[widget.communityId];
-      if (c == null) {
-        continue;
-      }
-
-      if (m.isAdmin) {
-        isAdmin = true;
-      }
-    }
 
     Widget tile = CupertinoListTile.notched(
       key: Key(index.toString()),
@@ -357,11 +183,6 @@ class _KarmaCoinUserSelectorState extends State<KarmaCoinUserSelector> {
             }
           : null,
     );
-
-    if (!isAdmin) {
-      return GestureDetector(
-          onLongPress: () => _showContextMenu(context, index), child: tile);
-    }
 
     return tile;
   }
@@ -478,9 +299,12 @@ class _KarmaCoinUserSelectorState extends State<KarmaCoinUserSelector> {
               ),
             ),
           ),
+          _getSearchBar(context),
           SliverFillRemaining(
             hasScrollBody: true,
-            child: _getBodyContent(context),
+            child: CustomScrollView(
+              slivers: [_getContactsList(context)],
+            ),
           ),
         ],
       ),
