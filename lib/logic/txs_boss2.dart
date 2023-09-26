1a34859ec2ef6af8222bb97d04112d4edabb62e3
avive <aviveyal07@gmail.com>
2023-09-26 09:25:36 +0300

    Display pool metadata in pools ui

--- lib/logic/txs_boss2.dart
+++ lib/logic/txs_boss2.dart
@@ -9,7 +9,7 @@ class KC2TransactionBoss extends KC2TransactionBossInterface {
   @override
   void addAppreciation(KC2AppreciationTxV1 tx) {
     // we need to replace the observeable list for clients to get notified
-    debugPrint('Adding appreciation to user txs: ${tx.hash}');
+    debugPrint('>>> Adding appreciation to user txs: ${tx.hash}');
 
     if (tx.toAccountId == accountId) {
       Map<String, KC2Tx> txs = {};
@@ -33,7 +33,7 @@ class KC2TransactionBoss extends KC2TransactionBossInterface {
   @override
   void addTransferTx(KC2TransferTxV1 tx) {
     // we need to replace the observeable list for clients to get notified
-    debugPrint('Adding transfer to user txs: ${tx.hash}');
+    debugPrint('>>> Adding transfer to user txs: ${tx.hash}');
     if (tx.toAddress == accountId) {
       Map<String, KC2Tx> txs = {};
       txs.addAll(incomingAppreciations.value);
--- lib/ui/components/pool.dart
+++ lib/ui/components/pool.dart
@@ -31,209 +31,220 @@ class _PoolWidgetState extends State<PoolWidget> {
     return ValueListenableBuilder<PoolMember?>(
         valueListenable: kc2User.poolMembership,
         builder: (context, value, child) {
-          List<CupertinoListTile> tiles = [];
-          final pool = widget.pool;
+          try {
+            List<CupertinoListTile> tiles = [];
+            final pool = widget.pool;
 
-          if (pool.socialUrl != null) {
-            String url = pool.socialUrl!.startsWith("https://")
-                ? pool.socialUrl!
-                : "https://${pool.socialUrl}";
+            if (pool.metadata != null && pool.metadata!.isNotEmpty) {
+              String url = pool.metadata!.startsWith("https://")
+                  ? pool.metadata!
+                  : "https://${pool.metadata}";
 
-            tiles.add(CupertinoListTile.notched(
-              leading: const Icon(CupertinoIcons.globe, size: 28),
-              title: Text(
-                pool.socialUrl!,
-                style: CupertinoTheme.of(context)
-                    .textTheme
-                    .navTitleTextStyle
-                    .merge(
-                      const TextStyle(color: CupertinoColors.activeBlue),
-                    ),
-              ),
-              trailing: const CupertinoListTileChevron(),
-              onTap: () async {
-                await openUrl(url);
-              },
-            ));
-          }
-
-          tiles.add(CupertinoListTile.notched(
-            title: const Text('Stake'),
-            leading: const FaIcon(FontAwesomeIcons.coins, size: 24),
-            trailing: Text(
-              // todo: format it properly
-              pool.balance!.formatAmount(),
-            ),
-          ));
-
-          String members = pool.memberCounter == 1
-              ? '1 Member'
-              : '${KarmaCoinAmountFormatter.deicmalFormat.format(pool.memberCounter)} Members';
-
-          tiles.add(CupertinoListTile.notched(
-            title: Text(members),
-            leading: const FaIcon(FontAwesomeIcons.peopleGroup, size: 24),
-            trailing: pool.memberCounter > 1
-                ? const CupertinoListTileChevron()
-                : Container(),
-          ));
-
-          KC2UserInfo? creator = pool.depositor;
-
-          if (creator != null) {
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Creator'),
-              leading: RandomAvatar(creator.userName, height: 50, width: 50),
-              subtitle: Text(
-                  // todo: format this properly
-                  creator.userName),
-              trailing: const CupertinoListTileChevron(),
-              onTap: () => context.pushNamed(ScreenNames.account,
-                  params: {'accountId': creator.accountId}),
-            ));
-          }
-
-          KC2UserInfo? root = pool.root;
-          if (root != null) {
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Root'),
-              leading: RandomAvatar(root.userName, height: 50, width: 50),
-              subtitle: Text(
-                  // todo: format this properly
-                  root.userName),
-              trailing: const CupertinoListTileChevron(),
-              onTap: () => context.pushNamed(ScreenNames.account,
-                  params: {'accountId': root.accountId}),
-            ));
-          }
-
-          KC2UserInfo? nominator = pool.nominator;
-          if (nominator != null) {
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Nominator'),
-              leading: RandomAvatar(nominator.userName, height: 50, width: 50),
-              subtitle: Text(
-                  // todo: format this properly
-                  nominator.userName),
-              trailing: const CupertinoListTileChevron(),
-              onTap: () => context.pushNamed(ScreenNames.account,
-                  params: {'accountId': nominator.accountId}),
-            ));
-          }
-
-          KC2UserInfo? bouncer = pool.bouncer;
-          if (bouncer != null) {
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Bouncer'),
-              leading: RandomAvatar(bouncer.userName, height: 50, width: 50),
-              subtitle: Text(
-                  // todo: format this properly
-                  bouncer.userName),
-              trailing: const CupertinoListTileChevron(),
-              onTap: () => context.pushNamed(ScreenNames.account,
-                  params: {'accountId': bouncer.accountId}),
-            ));
-          }
-          final double commision = pool.commission.currentAsPercent ?? 0.0;
-          final String commisionString =
-              KarmaCoinAmountFormatter.deicmalFormat.format(commision * 100.0);
-
-          tiles.add(CupertinoListTile.notched(
-            title: const Text('Commision'),
-            leading: const FaIcon(FontAwesomeIcons.moneyBill, size: 24),
-            trailing: Text('$commisionString%'),
-          ));
-
-          KC2UserInfo? commisionBeneficiary = pool.commissionBeneficiary;
-          if (commisionBeneficiary != null) {
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Commision Beneficiery'),
-              leading: RandomAvatar(commisionBeneficiary.userName,
-                  height: 50, width: 50),
-              subtitle: Text(
-                  // todo: format this properly
-                  commisionBeneficiary.userName),
-              trailing: const CupertinoListTileChevron(),
-              onTap: () => context.pushNamed(ScreenNames.account,
-                  params: {'accountId': commisionBeneficiary.accountId}),
-            ));
-          }
-
-          final double? maxAsPrecent = pool.commission.maxAsPercent;
-          if (maxAsPrecent != null) {
-            final String maxCommision = KarmaCoinAmountFormatter.deicmalFormat
-                .format(maxAsPrecent * 100.0);
-
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Max Commision'),
-              leading: const FaIcon(FontAwesomeIcons.maximize, size: 24),
-              trailing: Text('$maxCommision%'),
-            ));
-          }
-
-          if (value != null && value.id == widget.pool.id) {
-            tiles.add(CupertinoListTile.notched(
-              title: const Text('Your Stake'),
-              leading: const FaIcon(FontAwesomeIcons.yinYang, size: 24),
-              trailing: Text(
-                value.points.formatAmount(),
-              ),
-            ));
-
-            if (kc2User.poolClaimableRewardAmount.value > BigInt.zero) {
               tiles.add(CupertinoListTile.notched(
-                title: const Text('Your Earnings'),
-                trailing: CupertinoButton(
-                  padding: const EdgeInsets.only(left: 0.0),
-                  onPressed: () {
-                    if (!context.mounted) return;
-                    Navigator.of(context).push(
-                      CupertinoPageRoute(
-                        fullscreenDialog: true,
-                        builder: ((context) =>
-                            ClaimPayout(pool: pool, membership: value)),
+                leading: const Icon(CupertinoIcons.globe, size: 28),
+                title: Text(
+                  url,
+                  style: CupertinoTheme.of(context)
+                      .textTheme
+                      .navTitleTextStyle
+                      .merge(
+                        const TextStyle(color: CupertinoColors.activeBlue),
                       ),
-                    );
-                  },
-                  child: const Text('Withdraw'),
                 ),
-                leading:
-                    const FaIcon(FontAwesomeIcons.moneyBillTrendUp, size: 24),
-                subtitle: Text(
-                    kc2User.poolClaimableRewardAmount.value.formatAmount()),
+                onTap: () async {
+                  await openUrl(url);
+                },
               ));
             } else {
               tiles.add(const CupertinoListTile.notched(
-                title: Padding(
-                    padding: EdgeInsets.only(), child: Text('Your Earnings')),
-                leading: FaIcon(FontAwesomeIcons.moneyBillTrendUp, size: 24),
-                subtitle: Text('No earnings yet'),
+                leading: Icon(CupertinoIcons.globe, size: 28),
+                title: Text("Pool has no website."),
               ));
             }
+
+            tiles.add(CupertinoListTile.notched(
+              title: const Text('Stake'),
+              leading: const FaIcon(FontAwesomeIcons.coins, size: 24),
+              trailing: Text(
+                // todo: format it properly
+                pool.balance!.formatAmount(),
+              ),
+            ));
+
+            String members = pool.memberCounter == 1
+                ? '1 Member'
+                : '${KarmaCoinAmountFormatter.deicmalFormat.format(pool.memberCounter)} Members';
+
+            tiles.add(CupertinoListTile.notched(
+              title: Text(members),
+              leading: const FaIcon(FontAwesomeIcons.peopleGroup, size: 24),
+              trailing: pool.memberCounter > 1
+                  ? const CupertinoListTileChevron()
+                  : Container(),
+            ));
+
+            KC2UserInfo? creator = pool.depositor;
+
+            if (creator != null) {
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Creator'),
+                leading: RandomAvatar(creator.userName, height: 50, width: 50),
+                subtitle: Text(
+                    // todo: format this properly
+                    creator.userName),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () => context.pushNamed(ScreenNames.account,
+                    params: {'accountId': creator.accountId}),
+              ));
+            }
+
+            KC2UserInfo? root = pool.root;
+            if (root != null) {
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Root'),
+                leading: RandomAvatar(root.userName, height: 50, width: 50),
+                subtitle: Text(
+                    // todo: format this properly
+                    root.userName),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () => context.pushNamed(ScreenNames.account,
+                    params: {'accountId': root.accountId}),
+              ));
+            }
+
+            KC2UserInfo? nominator = pool.nominator;
+            if (nominator != null) {
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Nominator'),
+                leading:
+                    RandomAvatar(nominator.userName, height: 50, width: 50),
+                subtitle: Text(
+                    // todo: format this properly
+                    nominator.userName),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () => context.pushNamed(ScreenNames.account,
+                    params: {'accountId': nominator.accountId}),
+              ));
+            }
+
+            KC2UserInfo? bouncer = pool.bouncer;
+            if (bouncer != null) {
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Bouncer'),
+                leading: RandomAvatar(bouncer.userName, height: 50, width: 50),
+                subtitle: Text(
+                    // todo: format this properly
+                    bouncer.userName),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () => context.pushNamed(ScreenNames.account,
+                    params: {'accountId': bouncer.accountId}),
+              ));
+            }
+            final double commision = pool.commission.currentAsPercent ?? 0.0;
+            final String commisionString = KarmaCoinAmountFormatter
+                .deicmalFormat
+                .format(commision * 100.0);
+
+            tiles.add(CupertinoListTile.notched(
+              title: const Text('Commision'),
+              leading: const FaIcon(FontAwesomeIcons.moneyBill, size: 24),
+              trailing: Text('$commisionString%'),
+            ));
+
+            KC2UserInfo? commisionBeneficiary = pool.commissionBeneficiary;
+            if (commisionBeneficiary != null) {
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Commision Beneficiery'),
+                leading: RandomAvatar(commisionBeneficiary.userName,
+                    height: 50, width: 50),
+                subtitle: Text(
+                    // todo: format this properly
+                    commisionBeneficiary.userName),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () => context.pushNamed(ScreenNames.account,
+                    params: {'accountId': commisionBeneficiary.accountId}),
+              ));
+            }
+
+            final double? maxAsPrecent = pool.commission.maxAsPercent;
+            if (maxAsPrecent != null) {
+              final String maxCommision = KarmaCoinAmountFormatter.deicmalFormat
+                  .format(maxAsPrecent * 100.0);
+
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Max Commision'),
+                leading: const FaIcon(FontAwesomeIcons.maximize, size: 24),
+                trailing: Text('$maxCommision%'),
+              ));
+            }
+
+            if (value != null && value.id == widget.pool.id) {
+              tiles.add(CupertinoListTile.notched(
+                title: const Text('Your Stake'),
+                leading: const FaIcon(FontAwesomeIcons.yinYang, size: 24),
+                trailing: Text(
+                  value.points.formatAmount(),
+                ),
+              ));
+
+              if (kc2User.poolClaimableRewardAmount.value > BigInt.zero) {
+                tiles.add(CupertinoListTile.notched(
+                  title: const Text('Your Earnings'),
+                  trailing: CupertinoButton(
+                    padding: const EdgeInsets.only(left: 0.0),
+                    onPressed: () {
+                      if (!context.mounted) return;
+                      Navigator.of(context).push(
+                        CupertinoPageRoute(
+                          fullscreenDialog: true,
+                          builder: ((context) =>
+                              ClaimPayout(pool: pool, membership: value)),
+                        ),
+                      );
+                    },
+                    child: const Text('Withdraw'),
+                  ),
+                  leading:
+                      const FaIcon(FontAwesomeIcons.moneyBillTrendUp, size: 24),
+                  subtitle: Text(
+                      kc2User.poolClaimableRewardAmount.value.formatAmount()),
+                ));
+              } else {
+                tiles.add(const CupertinoListTile.notched(
+                  title: Padding(
+                      padding: EdgeInsets.only(), child: Text('Your Earnings')),
+                  leading: FaIcon(FontAwesomeIcons.moneyBillTrendUp, size: 24),
+                  subtitle: Text('No earnings yet'),
+                ));
+              }
+            }
+
+            tiles.add(
+              CupertinoListTile.notched(
+                title: _getPoolActionButton(context, value),
+              ),
+            );
+
+            return CupertinoListSection.insetGrouped(
+                key: Key(pool.id.toString()),
+                header: widget.showHeader
+                    ? Text(
+                        'Pool ${pool.id.toString()}',
+                        style: CupertinoTheme.of(context)
+                            .textTheme
+                            .navTitleTextStyle
+                            .merge(
+                              const TextStyle(
+                                  fontSize: 20,
+                                  color: CupertinoColors.activeGreen),
+                            ),
+                      )
+                    : null,
+                children: tiles);
+          } catch (e) {
+            debugPrint('Error rendering pool: $e');
+            return Container();
           }
-
-          tiles.add(
-            CupertinoListTile.notched(
-              title: _getPoolActionButton(context, value),
-            ),
-          );
-
-          return CupertinoListSection.insetGrouped(
-              key: Key(pool.id.toString()),
-              header: widget.showHeader
-                  ? Text(
-                      'Pool ${pool.id.toString()}',
-                      style: CupertinoTheme.of(context)
-                          .textTheme
-                          .navTitleTextStyle
-                          .merge(
-                            const TextStyle(
-                                fontSize: 20,
-                                color: CupertinoColors.activeGreen),
-                          ),
-                    )
-                  : null,
-              children: tiles);
         });
   }
 
